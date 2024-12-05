5c89e4f5141115d075566be5b39e4c17effdbeac
Jetson Nano <spex@rit.edu>
2024-12-04 21:26:13 -0500

    cleaned up xbee_node

--- src/communications/xbee/xbee/xbee_node.py
+++ src/communications/xbee/xbee/xbee_node.py
@@ -12,10 +12,14 @@ import time
 import rclpy
 from rclpy.node import Node
 from rclpy.publisher import Publisher
+
 # from custom_interfaces.msg import CanFD, Can
 from constants.CommanndCodes import TOPICS_JOYSTICK, TOPICS_BUTTON
 import rclpy.publisher
 import rclpy.subscription
+import xbee
+import xbee.xbee
+import xbee.xbee.xbee_node
 
 XBEE_PORT = "/dev/ttyUSB0"
 XBEE_SPEED = 921600
@@ -24,152 +28,49 @@ XBEE_TIMEOUT = 1000000000  # 1,000,000 nano second -> 1 second
 
 
 class Xbee(Node):
-    def __init__(self):
+    # flag to determine if the xbee should be disabled based on no signal
+    __disabled: bool
 
+    # flag to be triggered once the xbee has received any data
+    __is_first_connected: bool
+
+    # all the current values from the xbee
+    # self.__button_values = [False] * CONSTANTS.NUM_BUTTONS
+    __axis_values: list[float]
+
+    # open the port to the device
+    __xbee_device: XBeeDevice
+
+    # track when the last successful message was received
+    __last_successful_message: int
+
+    def __init__(self):
         super().__init__("Xbee_node")
 
-        # the number of iterations without signal
-        self.__num_no_signal = 0
-
-        # flag to determine if the xbee should be disabled based
-        # on no signal
-        self.__is_disabled = False
-
-        # flag to be triggered once the xbee has received any data
+        self.__disabled = False
         self.__is_first_connected = False
-
-        # all the current values from the xbee
-        # self.__button_values = [False] * CONSTANTS.NUM_BUTTONS
         self.__axis_values = [0.0] * CONSTANTS.NUM_AXES
-
-        # open the port to the device
         self.__xbee_device = XBeeDevice(XBEE_PORT, XBEE_SPEED)
         self.__xbee_device.open()
-
-        # track when the last successful message was received
         self.__last_successful_message = time.time_ns()
 
-
     def __del__(self):
-        """
-        have the device port be closed
-        """
+        # close device on deletion
         self.__xbee_device.close()
 
-    # """
-    # get the current value of selected input
-
-    # :param input_type - Specifies what type of input
-    # :param input_trigger - Controller button or axis, value from CommandCodes
-    # """
-
-    # def get_current_value(self, input_type: int, input_trigger: int) -> float | bool:
-    #     match input_type:
-    #         case CONSTANTS.INPUT_TYPE.IS_AXIS:
-    #             if input_trigger == CONSTANTS.JOYSTICK.AXIS_LY:
-    #                 return self.__axis_values[0]
-    #             else:
-    #                 return self.__axis_values[1]
-    #         case CONSTANTS.INPUT_TYPE.IS_AXIS:
-    #             return self.__button_values[input_trigger]
-    #         case CONSTANTS.INPUT_TYPE.IS_BUTTON:
-    #             return self.__button_values[
-    #                 CONSTANTS.NUM_BUTTONS
-    #                 - CONSTANTS.NUM_TRIGGER
-    #                 + (input_trigger - CONSTANTS.NUM_AXES)
-    #             ]
-
-    #     return False
-
-    # print all the current values to terminal
-    # def print_values(self) -> None:
-
-    #     print(self.__button_values)
-
-    #     print(
-    #         "Left Axis: ",
-    #         self.get_current_value(
-    #             CONSTANTS.INPUT_TYPE.IS_AXIS, CONSTANTS.JOYSTICK.AXIS_LY
-    #         ),
-    #     )
-
-    #     print(
-    #         " Right Axis: ",
-    #         self.get_current_value(
-    #             CONSTANTS.INPUT_TYPE.IS_AXIS, CONSTANTS.JOYSTICK.AXIS_RY
-    #         ),
-    #     )
-
-    #     print(
-    #         " A Button: ",
-    #         self.get_current_value(CONSTANTS.INPUT_TYPE.IS_BUTTON, CONSTANTS.BUTTONS.A),
-    #     )
-
-    #     print(
-    #         " B Button: ",
-    #         self.get_current_value(CONSTANTS.INPUT_TYPE.IS_BUTTON, CONSTANTS.BUTTONS.B),
-    #     )
-
-    #     print(
-    #         " X Button: ",
-    #         self.get_current_value(CONSTANTS.INPUT_TYPE.IS_BUTTON, CONSTANTS.BUTTONS.X),
-    #     )
-
-    #     print(
-    #         " Y Button: ",
-    #         self.get_current_value(CONSTANTS.INPUT_TYPE.IS_BUTTON, CONSTANTS.BUTTONS.Y),
-    #     )
-
-    #     print(
-    #         " LB Button: ",
-    #         self.get_current_value(
-    #             CONSTANTS.INPUT_TYPE.IS_BUTTON, CONSTANTS.BUTTONS.LEFT_BUMPER
-    #         ),
-    #     )
-
-    #     print(
-    #         " RB Button: ",
-    #         self.get_current_value(
-    #             CONSTANTS.INPUT_TYPE.IS_BUTTON, CONSTANTS.BUTTONS.RIGHT_BUMPER
-    #         ),
-    #     )
-
-    #     print(
-    #         " LT Button: ",
-    #         self.get_current_value(
-    #             CONSTANTS.INPUT_TYPE.IS_TRIGGER, CONSTANTS.TRIGGER.AXIS_LT
-    #         ),
-    #     )
-
-    #     print(
-    #         " RT Button: ",
-    #         self.get_current_value(
-    #             CONSTANTS.INPUT_TYPE.IS_TRIGGER, CONSTANTS.TRIGGER.AXIS_RT
-    #         ),
-    #     )
-
-    #     print()
-
-    # checks if the xbee is disabled
     def is_disabled(self) -> bool:
-        return self.__is_disabled
+        return self.__disabled
 
-    # clears the disable flag to allow the xbee to continue normal function
-    def clear_disable(self) -> None:
-        self.__is_disabled = False
+    def set_disabled(self, disabled: bool):
+        self.__disabled = disabled
 
-    # disable the xbee
-    def disable_xbee(self) -> None:
-        self.__is_disabled = True
-
-
-    #
     def __parse_incoming_message(self, message: list[int]):
         """
         helper function that is called when message is received, to parse to get values
 
         :param message - a full message that start with start message
         """
+
         # the current byte number
         byte_num: int = 0
 
@@ -206,23 +107,23 @@ class Xbee(Node):
             self.create_publisher(
                 TOPICS_BUTTON[i]["val"],
                 f"/Xbee/Buttons/RX/{TOPICS_BUTTON[i]['name']}",
-                10
+                10,
             ).publish(button_value)
 
     def send_msg(self):
-        pass
+        raise NotImplementedError
 
     def on_message_received(self):
         """
         callback function that is called when message is received
         """
-        # xbee is disabled
-        if self.__is_disabled:
+
+        # stop if xbee is disabed
+        if self.__disabled:
             return
 
-        message = None
-
         # get message from the physical xbee
+        message = None
         try:
             message = self.__xbee_device.read_data(0.0004)
         except TimeoutException:
@@ -232,65 +133,26 @@ class Xbee(Node):
             print(e)
             return
 
-        # message is invalid
+        # stop if message is none
         if message is None:
             return
 
-        # check if message has a valid start message
+        # if start of message is not valid, stop
         if list(message.data)[0] != int.from_bytes(CONSTANTS.START_MESSAGE, "big"):
             return
-        else:
-            if not self.__is_first_connected:
-                self.__is_first_connected = True
 
-            self.__parse_incoming_message(list(message.data)[1:])
-            # self.print_values()
+        if not self.__is_first_connected:
+            self.__is_first_connected = True
 
-            self.__last_successful_message = time.time_ns()
+        self.__parse_incoming_message(list(message.data)[1:])
 
-            # message_id = 3
-
-            # ros_msg = Can()
-            # ros_msg.channel = TOPICS[message_id]["channel"]
-            # ros_msg.id = message_id
-            # ros_msg.buf = [
-            #     int(
-            #         (
-            #             self.get_current_value(
-            #                 CONSTANTS.JOYSTICK.AXIS_LY, CONSTANTS.INPUT_TYPE.IS_AXIS
-            #             )
-            #             * 100
-            #         )
-            #         + 100
-            #     ),
-            #     int(
-            #         (
-            #             self.get_current_value(
-            #                 CONSTANTS.JOYSTICK.AXIS_RY, CONSTANTS.INPUT_TYPE.IS_AXIS
-            #             )
-            #             * 100
-            #         )
-            #         + 100
-            #     ),
-            #     0,
-            #     0,
-            #     0,
-            #     0,
-            #     0,
-            #     0,
-            # ]
-
-            # self.create_publisher(
-            #     msg_type=Can,
-            #     topic=f"/CAN/TX/{TOPICS[message_id]['name']}",
-            #     qos_profile=10,
-            # ).publish(ros_msg)
+        self.__last_successful_message = time.time_ns()
 
     def run(self):
         # rclpy.spin(self)
         last_cycle_time = time.time_ns()
 
-        while not self.__is_disabled:
+        while not self.__disabled:
             if time.time_ns() - last_cycle_time > XBEE_UPDATE_RATE:
                 last_cycle_time = time.time_ns()
                 self.on_message_received()
@@ -299,7 +161,7 @@ class Xbee(Node):
                     time.time_ns() - self.__last_successful_message > XBEE_TIMEOUT
                     and self.__is_first_connected
                 ):
-                    self.disable_xbee()
+                    self.set_disabled(True)
 
 
 def main():
