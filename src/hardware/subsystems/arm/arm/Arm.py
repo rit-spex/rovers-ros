8973873b321e46faae9c4d4f8ab06814ae2de844
Garrett59100 <garrett59100@gmail.com>
2025-01-26 14:00:14 -0500

    some things for arm

--- src/hardware/subsystems/arm/arm/Arm.py
+++ src/hardware/subsystems/arm/arm/Arm.py
@@ -1,6 +1,26 @@
-def main():
-    print('Hi from arm.')
+import rclpy
+from rclpy import Node, subscription
+from custom_interfaces.msg import ArmMotor
 
+class Arm(Node):
+    # Base
+    base_spd: subscription
+    base_dir: subscription
+    # Gripper
+    grip_spd: subscription
+    grip_dir: subscription
+    # Wrist
+    wrst_spd: subscription
+    wrst_dir: subscription
 
-if __name__ == '__main__':
-    main()
+    def __init__(self) -> None:
+        super.__init__('Arm')
+
+        self.base_spd = self.create_subscription(ArmMotor, "/ARM/BASE/SPEED", lambda x:
+            1
+        )
+        self.base_dir = self.create_subscription(ArmMotor, "/ARM/BASE/DIRECTION")
+        self.grip_spd = self.create_subscription(ArmMotor, "/ARM/GRIPPER/SPEED")
+        self.grip_dir = self.create_subscription(ArmMotor, "/ARM/GRIPPER/DIRECTION")
+        self.wrst_spd = self.create_subscription(ArmMotor, "/ARM/WRIST/SPEED")
+        self.wrst_dir = self.create_subscription(ArmMotor, "/ARM/WRIST/DIRECTION")
