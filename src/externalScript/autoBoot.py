7350ea536379919f9efcdb8a64b07f1ed7639632
22savagelol <jkjgoldfish@icloud.com>
2025-09-30 09:35:51 -0400

    Trying to get python to change path then running ros2 launch

--- src/externalScript/autoBoot.py
+++ src/externalScript/autoBoot.py
@@ -14,7 +14,7 @@ finally:
 
 
 try:
-    scriptLine = ['bash', 'source.sh']#, 'ros2', 'launch', 'main', 'main_launch.xml']
+    scriptLine = ['bash', 'launchRos2']#, 'ros2', 'launch', 'main', 'main_launch.xml']
     result = subprocess.run(scriptLine, capture_output=True, text=True, check=True)
     print(scriptLine)
     print("Script output:")
@@ -26,9 +26,16 @@ except subprocess.CalledProcessError as e:
     print(f"Error executing script: {e}")
     print(f"Stderr: {e.stderr}")
 
+#try:
+#    os.chdir("/home/savage22/ros/rovers-ros/")
+#except FileNotFoundError as e:
+#    print(f"Error: {e}")
+#finally:
+#    print("Current Directory:", os.getcwd())
+
 
 #try:
-#    result = subprocess.run(['ros2','launch', 'main', 'main_launch.xml'])
+#    result = subprocess.run(['bash', 'launchRos2'])
 #    print("Script output:")
 #    print(result.stdout)
 #    if result.stderr:
