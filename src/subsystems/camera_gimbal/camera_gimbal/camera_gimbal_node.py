c7e558361365247ca697152462bfb0cf8f4f6b61
MFZ8566 <mfz8566@rit.edu>
2026-01-29 21:53:50 -0500

    Set up preliminary initialization

--- src/subsystems/camera_gimbal/camera_gimbal/camera_gimbal_node.py
+++ src/subsystems/camera_gimbal/camera_gimbal/camera_gimbal_node.py
@@ -3,33 +3,41 @@ from constants.CAN_Constants import TOPICS
 from constants.CommandCodes import CONSTANTS
 
 import rclpy
-from rclpy.node import Node, Timer
+from rclpy.node import Node
 from rclpy.publisher import Publisher
-from std_msgs.msg import Bool, Float32, UInt8MultiArray, Int8, Int16
-import Jetson.GPIO as GPIO
-
-LED_flicker_sec = 1.0
-LED_PIN = 7
-
+from std_msgs.msg import Bool, Float32, UInt8MultiArray, Int8, Int16, Int32
+from Jetson.
+from STorM32_lib import *
 
 class CameraGimbal(Node):
 
-    LED_flicker_timer: Timer
+    # Configuration
+    baud : Int32 = 115200
+    UART_port = "COM3" 
 
-    # gpio status
-    __led_status: bool
+    # Camera Control Inputs
+    yaw_deg : Float32
+    pitch_deg : Float32
+    roll_deg : Float32
+    # Camera Control Limits
+    yaw_max : Float32 = 45.0
+    yaw_min : Float32 = -45.0
+    pitch_max : Float32 = 90.0
+    pitch_min : Float32 = -90.0
+    roll_max : Float32 = 45.0
+    roll_min : Float32 = -45.0
 
     def __init__(self):
+        # Initialize ROS node
         super().__init__("camera_gimbal_node")
 
-        # GPIO setup to have led indicate heartbeat status
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
-        # GPIO.output(LED_PIN, GPIO.LOW)
+        # Start serial
+        ser = serial.Serial(UART_port, baud)
 
-        # requires some default value
-        self.__led_status = False
+        # Set initial angles
+        reset_angles()
 
+        # Subscribe to ESTOP
         self.create_subscription(
             msg_type=Bool,
             topic="/ESTOP",
@@ -37,24 +45,14 @@ class CameraGimbal(Node):
             qos_profile=10,
         )
 
-        self.LED_flicker_timer = self.create_timer(
-            timer_period_sec=LED_flicker_sec,
-            callback=self.__flicker_led,
-        )
-
-    def __flicker_led(self):
-        # self.get_logger().info("flicker_led")
-        if self.__led_status == True:
-            self.__led_status = False
-            GPIO.output(LED_PIN, GPIO.LOW)  # Turn off LED to indicate live jetson
-        else:
-            self.__led_status = True
-            GPIO.output(LED_PIN, GPIO.HIGH)  # Turn on LED to indicate live jetson
+    def reset_angles():
+        self.yaw_deg = 0.0
+        self.pitch_deg = 0.0
+        self.roll_deg = 0.0
 
     def __on_estop_received(self, msg: Bool):
         self.get_logger().info("E-STOP received, stopping status LED...")
-        self.__led_status = True
-        GPIO.output(LED_PIN, GPIO.HIGH)  # Turn the LED solid to indicate e-stop
+        
         rclpy.shutdown()
 
     def run(self):
@@ -65,8 +63,8 @@ class CameraGimbal(Node):
 
 def main():
     rclpy.init()
-    status_led = StatusLED()
-    status_led.run()
+    camera_gimbal = CameraGimbal()
+    camera_gimbal.run()
 
 
 if __name__ == "__main__":
